0b419d7c1d45b229dcc80953198ad4e00cb3e988
gsfish <caoyu97@hotmail.com>
2018-07-04 21:14:28 +0800

    use bpf filter

--- scan.py
+++ scan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import random
 import struct
 import argparse
@@ -10,44 +11,16 @@ from multiprocessing import Pool, Queue
 
 import netaddr
 from Crypto import Random
-from Crypto.Cipher import AES
 from scapy.all import *
 
 
-res_queue = Queue()
+
+def get_tcp_seq(validation):
+    return struct.unpack('>I', validation[:4])[0]
 
 
-def aes_enc(plain, key, iv):
-    cryptor = AES.new(key, AES.MODE_CBC, iv)
-    cipher = cryptor.encrypt(plain)
-    return cipher
-
-
-def aes_dec(cipher, key, iv):
-    cryptor = AES.new(key, AES.MODE_CBC, iv)
-    plain = cryptor.decrypt(cipher)
-    return plain
-
-
-def pad(raw, block_size, val):
-    pad_len = block_size - (len(raw) % block_size)
-    if pad_len % block_size == 0:
-        padded = raw
-    else:
-        padded = raw + bytearray([val] * pad_len)
-    return padded
-
-
-def validation_gen(dst, key, iv):
-    b_src = b'\x7f\x00\x00\x01'
-    b_dst = struct.pack(">I", int(hex(dst),16))
-    padded = pad(b_src+b_dst, AES.block_size, 0)
-    validation = aes_enc(padded, key, iv)
-    return validation
-
-
-def get_src_port(validation):
-    return struct.unpack('>I', validation[-4:])[0] % 0x10000
+def get_src_port(dport, validation):
+    return (struct.unpack('>I', validation[-4:])[0] + dport) % 0x10000
 
 
 def check_dport(dport, validation):
@@ -67,6 +40,8 @@ def synscan_check_pkt(pkt, sport_lst, validation):
         return False
     if pkt[TCP].sport not in sport_lst:
         return False
+    pkt.show()
+    return True
     if not check_dport(pkt[TCP].dport, validation):
         return False
     if pkt[TCP].ack != struct.unpack('>I', validation[:4])[0] + 1:
@@ -74,22 +49,24 @@ def synscan_check_pkt(pkt, sport_lst, validation):
     return True
 
 
-@args_storage(queue=res_queue)
-def on_ack(pkt, queue):
-    # print(pkt.sprintf('[+] %IP.src%:%IP.sport% open'))
-    queue.put((pkt[IP].src, pkt[IP].sport))
+def on_ack(pkt):
+    print(pkt.sprintf('[+] %IP.src%:%IP.sport% open'))
 
 
 def pkt_send(dst, dport, validation, **kwargs):
-    tcp_seq = struct.unpack('>I', validation[:4])[0]
-    tcp_sport = get_src_port(validation)
+    print('[*] scanning {0}'.format(dst))
+    tcp_seq = get_tcp_seq(validation)
+    tcp_sport = get_src_port(dport, validation)
     pkt = IP(dst=str(dst)) / TCP(flags='S', seq=tcp_seq, sport=tcp_sport, dport=dport)
     send(pkt, **kwargs)
 
 
 def pkt_recv(sport_lst, validation):
-    synscan_filter = args_storage(sport_lst=sport_lst, validation=validation)(synscan_check_pkt)
-    sniff(lfilter=synscan_filter, prn=on_ack)
+    # synscan_filter = args_storage(sport_lst=sport_lst, validation=validation)(synscan_check_pkt)
+    # sniff(lfilter=synscan_filter, prn=on_ack)
+    valid_ack = get_tcp_seq(validation) + 1
+    synscan_filter = 'tcp and tcp[8:4] == {0}'.format(valid_ack)
+    sniff(filter=synscan_filter, prn=on_ack)
 
 
 def parse_args():
@@ -148,26 +125,21 @@ def check_process(value):
 
 
 def main():
+    if os.geteuid() != 0:
+        print('[!] please run as root')
+        exit(1)
+
     args = parse_args()
-
-    val_key = Random.new().read(32)
-    print('[+] val_key:{0}'.format(b2a_hex(val_key)))
-
-    aes_key = Random.new().read(16)
-    aes_iv = Random.new().read(16)
-    print('[+] aes_key:{0} aes_iv:{1}'.format(b2a_hex(aes_key), b2a_hex(aes_iv)))
+    validation = Random.new().read(8)
+    print('[+] validation: {0}'.format(b2a_hex(validation)))
 
     pool = Pool(args.process+1)
+    pool.apply_async(pkt_recv, args=(args.ports, validation))
     for host in args.host:
-        print('[*] scanning {0}'.format(host))
-        validation = validation_gen(host, aes_key, aes_iv)
-        pool.apply_async(pkt_recv, args=(args.ports, validation))
         for dport in args.ports:
-            pool.apply_async(pkt_send, args=(host, dport, validation), kwds={'inter': args.interval, 'verbose': args.verbose})
-    print('[*] receiving')
-    while True:
-        res = res_queue.get()
-        print('[+] {0}:{1} open'.format(res[0], res[1]))
+            pool.apply_async(pkt_send, args=(host, dport, validation), kwds={'verbose': args.verbose})
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
